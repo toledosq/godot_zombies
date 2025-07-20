cfb3e9aab95ca91b2741bf044b7ddb828b542625
toledosq <smith_chris93@hotmail.com>
2025-07-19 23:34:30 -0600

    Add log messages to inventory component

--- scripts/inventory_component.gd
+++ scripts/inventory_component.gd
@@ -9,14 +9,34 @@ class_name InventoryComponent extends Node
 	get:
 		return max_slots
 
+func _ready() -> void:
+	print("InventoryComponent: Max Slots = %d" % max_slots)
+	
 func add_item(item: ItemData, quantity: int = 1) -> Dictionary:
+	print("InventoryComponent: Adding item to inventory")
 	return inventory.add_item(item, quantity)
 
 func remove_item(item: ItemData, quantity: int = 1) -> bool:
+	print("InventoryComponent: Removing item from inventory")
 	return inventory.remove_item(item, quantity)
 
 func has_space_for(item: ItemData, quantity: int = 1) -> bool:
+	print("InventoryComponent: Checking inventory for space")
 	return inventory.has_space_for(item, quantity)
 
 func get_quantity(item: ItemData) -> int:
-	return inventory.get_quantity(item)
+	var quantity = inventory.get_quantity(item)
+	print("InventoryComponent: %d/%d items in inventory" % [])
+	return quantity
+
+func save(path: String) -> void:
+	print("InventoryComponent: Saving inventory to %s" % path)
+	ResourceSaver.save(inventory, path)
+
+func load(path: String) -> void:
+	var loaded_inventory = ResourceLoader.load(path)
+	if loaded_inventory:
+		inventory = loaded_inventory
+		print("InventoryComponent: Loaded saved inventory")
+	else:
+		print("InventoryComponent: Could not find saved inventory")
