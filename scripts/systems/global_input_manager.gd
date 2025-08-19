e2d69ec9597d614396d4007266e9f36ed18d4f42
toledosq <smith_chris93@hotmail.com>
2025-08-19 12:14:08 -0600

    Add ui_cancel handler

--- scripts/systems/global_input_manager.gd
+++ scripts/systems/global_input_manager.gd
@@ -1,1 +1,12 @@
 class_name GlobalInputManager extends Node
+
+signal ui_cancel_requested
+
+func _ready() -> void:
+	# Receive input even when tree is paused
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		ui_cancel_requested.emit()
+		get_tree().set_input_as_handled()
