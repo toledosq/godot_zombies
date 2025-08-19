6df8a04a9e72a517a951cd95e2ee329ec4e861be
toledosq <smith_chris93@hotmail.com>
2025-08-19 12:12:14 -0600

    Add signals to pause menu buttons

--- scripts/ui/pause_overlay.gd
+++ scripts/ui/pause_overlay.gd
@@ -6,14 +6,37 @@ class_name PauseMenu extends CanvasLayer
 @onready var settings_button: Button = $CenterContainer/Panel/VBoxContainer/SettingsButton
 @onready var quit_button: Button = $CenterContainer/Panel/VBoxContainer/QuitButton
 
-signal pause_toggle_requested
-
+signal continue_game_requested
+signal save_game_requested
+signal load_game_requested
+signal settings_menu_requested
+signal quit_game_requested
 
 func _ready() -> void:
 	# Receive input even when tree is paused
 	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+	
+	# Hide Menu
+	self.visible = false
+	
+	# Connect Buttons
+	continue_button.pressed.connect(_on_continue_pressed)
+	save_button.pressed.connect(_on_save_pressed)
+	load_button.pressed.connect(_on_load_pressed)
+	settings_button.pressed.connect(_on_settings_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		pause_toggle_requested.emit()
-		get_tree().set_input_as_handled()
+func _on_continue_pressed() -> void:
+	continue_game_requested.emit()
+
+func _on_quit_pressed() -> void:
+	quit_game_requested.emit()
+
+func _on_save_pressed() -> void:
+	save_game_requested.emit()
+
+func _on_load_pressed() -> void:
+	load_game_requested.emit()
+
+func _on_settings_pressed() -> void:
+	settings_menu_requested.emit()
